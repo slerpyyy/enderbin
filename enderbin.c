c80e96ecc375a9936e102ca369e4472ded0eb690
slerpyyy <aaron.bies@googlemail.com>
2019-10-03 16:31:38 +0200

    It can die now

--- enderbin.c
+++ enderbin.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
+#include <sys/stat.h>
 
-const char* flag = "AAAAAAXX\x03";
-const int flaglen = 8;
+const char* flag = "::ENDER::FLAG::\x05";
+const int flaglen = 15;
 
 int main(int argc, char* argv[])
 {
-	int num = *(flag + flaglen);
-	printf("num: %d\n", num);
+	srand(time(NULL));
 
-	// change file
+	int num = *(flag + flaglen);
+	//printf("num: %d\n", num);
 
 	if(argc < 1)
 	{
@@ -18,10 +20,21 @@ int main(int argc, char* argv[])
 		exit(-1);
 	}
 
-	FILE *infile = fopen((const char *)argv[0], "r");
-	FILE *outfile = fopen("tmp", "w");
+	if(num < 1)
+	{
+		remove(argv[0]);
+		puts("AAAAAAAAAAAAAAAA!!!!");
+		exit(0);
+	}
+
+	int tmp_name_len = strlen(argv[0]) + 12;
+	char* tmp_name = (char *)malloc(sizeof(char) * tmp_name_len);
+	sprintf(tmp_name, "%s.tmp%08X", argv[0], rand());
+
+	FILE *in_file = fopen(argv[0], "r");
+	FILE *out_file = fopen(tmp_name, "w");
 	
-	if(infile == NULL || outfile == NULL)
+	if(in_file == NULL || out_file == NULL)
 	{
 		puts("wat");
 		exit(-1);
@@ -30,24 +43,22 @@ int main(int argc, char* argv[])
 	int curr = 0;
 	int prog = 0;
 
-	while((curr = fgetc(infile)) != EOF)
+	while((curr = fgetc(in_file)) != EOF)
 	{
-		if(prog == flaglen)
-		{
-			puts("flag found!!");
-			fputc(num + 1, outfile);
-		}
-		else
-		{
-			fputc(curr, outfile);
-		}
+		if(prog == flaglen)fputc(num - 1, out_file);
+		else fputc(curr, out_file);
 
 		if(curr == *(flag + prog))prog++;
 		else prog = 0;
 	}
 
-	fclose(infile);
-	fclose(outfile);
+	fclose(in_file);
+	fclose(out_file);
+
+	chmod(tmp_name, 0x1ed);
+	rename(tmp_name, argv[0]);
+
+	puts("OUGH!");
 
 	return 0;
 }
