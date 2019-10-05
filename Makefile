95775bdee3e7c0734d1c2b260581f455d35c27ee
slerpyyy <aaron.bies@googlemail.com>
2019-10-05 13:04:05 +0200

    Comments and cleaning up
    - all code blocks have comments now
    - added free calls for all used strings
    - added optimizer flag

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all: enderbin
 
 enderbin: enderbin.c
-	gcc enderbin.c -o ender -Wall -Wextra -Werror
+	gcc enderbin.c -o ender -03 -Wall -Wextra -Werror
--- enderbin.c
+++ enderbin.c
@@ -23,35 +23,44 @@ int main(int argc, char* argv[])
 	
 	srand(time(NULL) - health);
 
+	// Catch error that should never happen
+	// (it you remove this check, the compiler screams at you for never using argc)
 	if(argc < 1)
 	{
 		fprintf(stderr, "Something went horribly wrong\n");
 		exit(-1);
 	}
 
+	char* org_name = argv[0];
+
+
+	// Scream in pain and die
 	if(health < 1)
 	{
-		remove(argv[0]);
 		int death_index = rand() % death_len;
 		printf("%s (0 hp)\n", death[death_index]);
+
+		remove(org_name);
 		exit(0);
 	}
 
 
-
-	int tmp_name_len = strlen(argv[0]) + 12;
+	// Generate tmp name for code modification
+	int tmp_name_len = strlen(org_name) + 12;
 	char* tmp_name = (char *)malloc(sizeof(char) * tmp_name_len);
-	sprintf(tmp_name, "%s.tmp%08X", argv[0], rand());
+	sprintf(tmp_name, "%s.tmp%08X", org_name, rand());
 
 
-
-	FILE *in_file = fopen(argv[0], "r");
+	// Open program in read mode
+	FILE *in_file = fopen(org_name, "r");
 	if(in_file == NULL)
 	{
 		fprintf(stderr, "Could not open source file\n");
 		exit(-1);
 	}
 
+
+	// Create tmp file in write mode
 	FILE *out_file = fopen(tmp_name, "w");
 	if(out_file == NULL)
 	{
@@ -60,7 +69,8 @@ int main(int argc, char* argv[])
 	}
 
 
-
+	// Copy program data and change health
+	// (Note: This algorithm only works, if flag[0] != flag[1])
 	int curr = 0;
 	int prog = 0;
 
@@ -74,11 +84,13 @@ int main(int argc, char* argv[])
 		else prog = 0;
 	}
 
+
+	// Close both files
 	fclose(in_file);
 	fclose(out_file);
 
 
-
+	// Make the modified file executable
 	if(chmod(tmp_name, 0x1ed))
 	{
 		remove(tmp_name);
@@ -86,21 +98,24 @@ int main(int argc, char* argv[])
 		exit(-1);
 	}
 
-	if(rename(tmp_name, argv[0]))
+	// Overwrite the old executable
+	if(rename(tmp_name, org_name))
 	{
 		remove(tmp_name);
 		fprintf(stderr, "Could not replace source file\n");
 		exit(-1);
 	}
 
+	free(tmp_name);
 
 
+	// Scream in pain
 	int damage_index = rand() % damage_len;
 	printf("%s (%d hp)\n", damage[damage_index], health);
 
 
-
-	char* path;
+	// Find a directory to teleport to
+	char* mv_dir = NULL;
 	int seed = 1 | rand();
 	struct dirent* dent;
 	DIR* srcdir = opendir(".");
@@ -116,27 +131,43 @@ int main(int argc, char* argv[])
 		
 		if(!S_ISDIR(st.st_mode))continue;
 
-		if(seed & 1)path = dent->d_name;
+		if(seed & 1)
+		{
+			mv_dir = dent->d_name;
+		}
 
-		seed /= 2;
+		if((seed /= 2) == 0)break;
 	}
 
+	free(dent);
+	free(srcdir);
 
 
-	int name_start = 0;
-	int argv0_len = strlen(argv[0]);
+	// Isolate the name of the executable
+	char *bin_name = NULL;
+	int argv0_len = strlen(org_name);
 	for(int i=0; i < argv0_len - 1; i++)
 	{
-		if(*(argv[0]+i) == '/')name_start = i + 1;
+		if(*(org_name+i) == '/')
+		{
+			bin_name = org_name + i + 1;
+		}
 	}
 
-	int name_len = strlen(path) + (argv0_len - name_start) + 1;
-	char* name = (char *)malloc(sizeof(char) * name_len);
-	sprintf(name, "%s/%s", path, argv[0] + name_start);
 
-	printf("%s -> %s\n", argv[0], name);
+	// Move executable by renaming it
+	int mv_name_len = strlen(mv_dir) + strlen(bin_name) + 1;
+	char* mv_name = (char *)malloc(sizeof(char) * mv_name_len);
+	sprintf(mv_name, "%s/%s", mv_dir, bin_name);
+	//printf("%s -> %s\n", org_name, mv_name);
+	rename(org_name, mv_name);
 
-	rename(argv[0], name);
+
+	// Free everything
+	free(org_name);
+	free(bin_name);
+	free(mv_dir);
+	free(mv_name);
 
 	return 0;
 }
